28deab3f9f61cc62f0187f6932c58c1da0739176
fernando39357 <fernando39357@gmail.com>
2020-07-21 21:40:29 -0300

    Styles

--- src/index.js
+++ src/index.js
@@ -5,15 +5,34 @@ const Page = styled.SafeAreaView`
   flex: 1;
   justify-content: center;
   align-items: center;
-  background-color: #ccc;
+  background-color: #00aeef;
 `;
 const ButtonsView = styled.View`
   flex-direction: row;
+  height: 40px;
+  margin-top: 120px;
 `;
 const Image = styled.Image``;
-const Button = styled.TouchableOpacity``;
-const SecondsText = styled.Text``;
-const ButtonText = styled.Text``;
+const Button = styled.TouchableOpacity`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  background-color: #fff;
+  height: 40px;
+  margin: 17px;
+  border-radius: 9px;
+`;
+const SecondsText = styled.Text`
+  margin-top: -160px;
+  color: #fff;
+  font-size: 65px;
+  font-weight: bold;
+`;
+const ButtonText = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+  color: #00aeef;
+`;
 
 export default function App() {
   return (
